411dacaf5cce3693a32c908ba444f77a7361fd6a
Overcomer <michael31703@gmail.com>
2020-05-26 22:13:47 +0800

    Add repeat code to __init__().

--- Computer Vision/Face/detection/Face_Detection.py
+++ Computer Vision/Face/detection/Face_Detection.py
@@ -3,18 +3,20 @@ import cv2
 import dlib
 
 class Face_Detect():
-    def haar_detect(self, haar_file=str(), image=None, show_image=False):
-        roi = None
-        face = None
+    def __init__(self, image=None):
+        self.roi = None
+        self.face = None
 
-        faceCascade = cv2.CascadeClassifier(haar_file)
-        
         # 判斷是否要讀取圖片
         if type(image) == str:
-            image = cv2.resize(cv2.imread(image), (640, 480))
-        copy_image = image.copy()
+            self.image = cv2.resize(cv2.imread(image), (640, 480))
+        self.copy_image = self.image.copy()
+    
+    def haar_detect(self, haar_file=str(), show_image=False):
+    
+        faceCascade = cv2.CascadeClassifier(haar_file)
 
-        gray = cv2.cvtColor(copy_image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(self.copy_image, cv2.COLOR_BGR2GRAY)
     
         faces = faceCascade.detectMultiScale(
             gray,
@@ -24,31 +26,22 @@ class Face_Detect():
         )
     
         # Draw a rectangle around the faces
-        for roi in faces:
-            (x, y, w, h) = roi
-            cv2.rectangle(copy_image, (x, y), (x+w, y+h), (255, 155, 0), 2)
-            face = image[y:y+h, x:x+w]
+        for self.roi in faces:
+            (x, y, w, h) = self.roi
+            cv2.rectangle(self.copy_image, (x, y), (x+w, y+h), (255, 155, 0), 2)
+            self.face = self.image[y:y+h, x:x+w]
 
             if show_image:
-                cv2.imshow('Haar Face', face)
-                cv2.imshow('Haar Original', copy_image)
+                cv2.imshow('Haar Face', self.face)
+                cv2.imshow('Haar Original', self.copy_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        return roi, image, face
-
     
-    def dlib_detect(self, image=None, show_image=False):
-        roi = None
-        face =None
-        
+    def dlib_detect(self, show_image=False):       
         detector = dlib.get_frontal_face_detector()
-        
-        # 判斷是否要讀取圖片
-        if type(image) == str:
-            image = cv2.resize(cv2.imread(image), (640, 480))
-        copy_image = image.copy()
-        faces = detector(copy_image, 1)
+
+        faces = detector(self.copy_image, 1)
 
         for face in faces:
             x = face.left()
@@ -56,24 +49,21 @@ class Face_Detect():
             w = face.right()
             h = face.bottom()
             # cv2.circle(copy_image, (int((x+w)/2), int((y+h)/2)), 100, (0, 155, 255), 2)
-            cv2.rectangle(copy_image, (x, y), (w, h), (255, 155, 0), 2)
+            cv2.rectangle(self.copy_image, (x, y), (w, h), (255, 155, 0), 2)
 
-            face = image[y:h, x:w]
+            self.face = self.image[y:h, x:w]
 
             if show_image:
-                cv2.imshow("Dlib Face", cv2.resize(face, (250, 250)))
-                cv2.imshow("Dlib Original", copy_image)
+                cv2.imshow("Dlib Face", cv2.resize(self.face, (250, 250)))
+                cv2.imshow("Dlib Original", self.copy_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-        return roi, image, face
         
 
     def dnn_detect(self, 
                    face_config=str(),
                    face_model=str(), 
                    dnn_framework="tensorflow",
-                   image=None, 
                    conf_threshold=0.7, 
                    show_image=False, 
                    ):
@@ -85,19 +75,10 @@ class Face_Detect():
         else:
             network = cv2.dnn.readNetFromTensorflow(face_model, face_config)
 
-        # 判斷是否要讀取圖片
-        if type(image) == str:
-            image = cv2.resize(cv2.imread(image), (640, 480))
-
-        # init roi and face
-        roi = None
-        face = None
-
-        copy_image = image.copy()
-        frameHeight = copy_image.shape[0]
-        frameWidth = copy_image.shape[1]
+        frameHeight = self.copy_image.shape[0]
+        frameWidth = self.copy_image.shape[1]
         # This image net to three chaneel image format.
-        blob = cv2.dnn.blobFromImage(copy_image, 1.0, (300, 300), [104, 117, 123], False, False)
+        blob = cv2.dnn.blobFromImage(self.copy_image, 1.0, (300, 300), [104, 117, 123], False, False)
 
         network.setInput(blob)
         detections = network.forward()
@@ -110,31 +91,28 @@ class Face_Detect():
                 h = int(detections[0, 0, i, 6] * frameHeight)
                 # face coordiante
                 if x or y or w or h:
-                    roi = [x, y, w, h]
-                    face = image[y:h, x:w]
-                    cv2.rectangle(copy_image, (x, y), (w, h), (255, 155, 0), 2)
+                    self.roi = [x, y, w, h]
+                    self.face = self.image[y:h, x:w]
+                    cv2.rectangle(self.copy_image, (x, y), (w, h), (255, 155, 0), 2)
+                    # if type(self.face) != type(None):
+                    if show_image:
+                        cv2.imshow("DNN face", cv2.resize(self.face, (250, 250)))
+                        cv2.imshow("DNN Original", self.copy_image)
                 else:
                     continue
-        if type(face) != type(None):
-            if show_image:
-                cv2.imshow("DNN face", cv2.resize(face, (250, 250)))
-                cv2.imshow("DNN Original", copy_image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-        return roi, image, face
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    detector = Face_Detect()
-    roi, image, face = detector.dnn_detect(face_config="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/dnn/opencv_face_detector.pbtxt",
-                                           face_model="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/dnn/opencv_face_detector_uint8.pb",
-                                           image="d:/2.jpg",
-                                           show_image=True
-                                          )
+    detector = Face_Detect("d:/2.jpg")
+    # detector.dnn_detect(face_config="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/dnn/opencv_face_detector.pbtxt",
+    #                     face_model="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/dnn/opencv_face_detector_uint8.pb",
+    #                     show_image=True
+    #                     )
     
-    roi, image, face = detector.dlib_detect(image="d:/2.jpg", show_image=True)
+    # detector.dlib_detect(show_image=True)
 
-    roi, image, face = detector.haar_detect(haar_file="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/haarcascade_frontalface_default.xml",
-                                            image="d:/2.jpg",
-                                            show_image=True)
+    detector.haar_detect(haar_file="F:/Master/Github/LBPH_Face_Recognition/models/face_detection/haarcascade_frontalface_default.xml",
+                        show_image=True)
 
